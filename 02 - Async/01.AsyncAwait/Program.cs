c3c88eded57cde015136f3cb2c20c4e34f0f70fb
VladislavZhukovsky <jaymzmanunited@gmail.com>
2015-11-25 16:12:42 +0300

    Async samples

--- 02 - Async/01.AsyncAwait/Program.cs
+++ 02 - Async/01.AsyncAwait/Program.cs
@@ -14,9 +14,9 @@ namespace _01.AsyncAwait
 
 
             var t = DoAsync();
-            for (var i = 0; i < 30; i++)
+            for (var i = 0; i < 50; i++)
             {
-                Console.WriteLine("MAIN" + Thread.CurrentContext.ContextID);
+                Console.WriteLine("111 MAIN" + Thread.CurrentThread.ManagedThreadId);
                 Thread.Sleep(1000);
             }
             Console.ReadKey();
@@ -24,22 +24,41 @@ namespace _01.AsyncAwait
 
         static async Task<int> DoAsync()
         {
+            //var t = Task.Run(() =>
+            //{
+            //    for (var i = 0; i < 10; i++)
+            //    {
+            //        Console.WriteLine("222 ASYNC" + Thread.CurrentThread.ManagedThreadId);
+            //        Thread.Sleep(1000);
+            //    }
+            //});
+            //await t;
+            Console.WriteLine("777 MAIN" + Thread.CurrentThread.ManagedThreadId);
+            await DoAsyncInner();
+            for (var i = 0; i < 30; i++)
+            {
+                Console.WriteLine("333 ASYNC REMAINDER" + Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(1000);
+            }
+            return 1000000;
+        }
+
+        static async Task DoAsyncInner()
+        {
             var t = Task.Run(() =>
             {
                 for (var i = 0; i < 10; i++)
                 {
-                    Console.WriteLine("ASYNC" + Thread.CurrentContext.ContextID.ToString());
+                    Console.WriteLine("444 ASYNC INNER" + Thread.CurrentThread.ManagedThreadId);
                     Thread.Sleep(1000);
                 }
             });
-
             await t;
             for (var i = 0; i < 10; i++)
             {
-                Console.WriteLine("NOT ASYNC" + Thread.CurrentContext.ContextID.ToString());
+                Console.WriteLine("555 ASYNC INNER REMAINDER" + Thread.CurrentThread.ManagedThreadId);
                 Thread.Sleep(1000);
             }
-            return 1000000;
         }
 
         static async Task DoVoidAsync()
