ff058ac58855550900a4bbd5496f56bd9e1cb373
VladislavZhukovsky <jaymzmanunited@gmail.com>
2015-12-08 16:19:48 +0300

    changes

---
+++ 03 - Expressions/Task01/Bar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+    public class Bar
+    {
+        //public Bar(int a)
+        //{
+        //    field = a;
+        //}
+
+        private int field;
+    }
+}
--- 03 - Expressions/Task01/Program.cs
+++ 03 - Expressions/Task01/Program.cs
@@ -11,48 +11,15 @@ namespace Task01
     {
         static void Main(string[] args)
         {
-            //var mapGenerator = new MappingGenerator();
-            //var mapper = mapGenerator.Generate<Foo, Bar>();
+            var mapGenerator = new MappingGenerator();
+            var mapper = mapGenerator.Generate<Foo, Bar>();
 
-            //var result = mapper.Map(new Foo());
-            var par = Expression.Parameter(typeof(int));
-            var add = Expression.Add(par, Expression.Constant(3));
-            var exp1 = Expression.New(typeof(object));
-            var l = Expression.Lambda<Func<object>>(exp1);
-            var ll = l.Compile();
-            var r = ll.Invoke();
-        }
-        public class Foo { }
-        public class Bar { }
-
-        public class Mapper<TSource, TDestination>
-        {
-            Func<TSource, TDestination> mapFunction;
-
-            internal Mapper(Func<TSource, TDestination> func)
-            {
-                mapFunction = func;
-            }
-
-            public TDestination Map(TSource source)
-            {
-                return mapFunction(source);
-            }
-        }
-
-        public class MappingGenerator
-        {
-            public Mapper<TSource, TDestination> Generate<TSource, TDestination>()
-            {
-                var sourceParam = Expression.Parameter(typeof(TSource));
-                var mapFunction =
-                    Expression.Lambda<Func<TSource, TDestination>>(
-                        Expression.New(typeof(TDestination)),
-                        sourceParam
-                 );
-                return new Mapper<TSource, TDestination>(mapFunction.Compile());
-            }
+            var result = mapper.Map(new Foo());
 
         }
+
+        
+
+        
     }
 }
