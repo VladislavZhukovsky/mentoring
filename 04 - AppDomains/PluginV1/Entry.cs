debd8341517eb4fb82a1a0902e2e7fee01e80929
VladislavZhukovsky <jaymzmanunited@gmail.com>
2015-12-15 03:17:13 +0300

    AppDomains  task.

---
+++ 04 - AppDomains/PluginV1/Entry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginV1
+{
+    public class Entry: MarshalByRefObject
+    {
+        public string DomainName { get; private set; }
+
+        public string PluginVersion { get; set; }
+
+        public Entry()
+        {
+            DomainName = AppDomain.CurrentDomain.FriendlyName;
+            PluginVersion = "v1";
+        }
+    }
+}
