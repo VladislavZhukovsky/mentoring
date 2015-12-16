5d201a0b2a6af053a4956284374d9ea340e4277d
VladislavZhukovsky <jaymzmanunited@gmail.com>
2015-12-17 02:37:10 +0300

    Unmanaged

---
+++ 05 - Unmanaged/Unmanaged/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unmanaged
+{
+    class Program
+    {
+        [DllImport("user32.dll", ThrowOnUnmappableChar = true)]
+        private static extern int MessageBox(
+            [In] IntPtr hWnd,
+            [MarshalAs(UnmanagedType.LPStr)] string text,
+            [MarshalAs(UnmanagedType.LPStr)] string caption,
+            int options);
+
+        static void Main(string[] args)
+        {
+            MessageBox(IntPtr.Zero, "Simple message box", "Window1", 0);
+        }
+    }
+}
