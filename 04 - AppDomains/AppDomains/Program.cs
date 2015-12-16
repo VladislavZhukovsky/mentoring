bf72ea76625c06ee450d84284a97f78e24dcf6fe
VladislavZhukovsky <jaymzmanunited@gmail.com>
2015-12-16 12:02:25 +0300

    updates

---
+++ 04 - AppDomains/AppDomains/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppDomains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void OtherDomainThreadCheck()
+        {
+            MessageBox.Show(AppDomain.CurrentDomain.FriendlyName + "Thread " + Thread.CurrentThread.ManagedThreadId);
+
+            var domainSetup = new AppDomainSetup()
+            {
+                ApplicationBase = AppDomain.CurrentDomain.BaseDirectory
+            };
+
+            var domain = AppDomain.CreateDomain("domainForOtherApp", new System.Security.Policy.Evidence(), domainSetup);
+            domain.ExecuteAssemblyByName("OtherApp");
+            AppDomain.Unload(domain);
+        }
+    }
+}
---
+++ 04 - AppDomains/OtherApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OtherApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageBox.Show(AppDomain.CurrentDomain.FriendlyName + "Thread " + Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
