5870c76878a4ff93532499548b7dd736bc56cef5
VladislavZhukovsky <jaymzmanunited@gmail.com>
2015-12-18 03:51:45 +0300

    COM

---
+++ 05 - Unmanaged/COM/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskScheduler.TaskScheduler sheduler = new TaskScheduler.TaskScheduler();
+            sheduler.Connect();
+
+            var folders = sheduler.GetFolder("\\Microsoft\\Windows\\WindowsUpdate");
+
+            foreach (TaskScheduler.IRegisteredTask task in folders.GetTasks((int)TaskScheduler._TASK_ENUM_FLAGS.TASK_ENUM_HIDDEN))
+            {
+                Console.WriteLine("{0} ({1}): {2} - {3}", task.Name, task.LastTaskResult, task.LastRunTime, task.NumberOfMissedRuns);
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- 05 - Unmanaged/Unmanaged/Program.cs
+++ 05 - Unmanaged/Unmanaged/Program.cs
@@ -19,6 +19,24 @@ namespace Unmanaged
         static void Main(string[] args)
         {
             MessageBox(IntPtr.Zero, "Simple message box", "Window1", 0);
+            var int32s = new Int32Struct();
+            int32s.Int = 5;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct Int32Struct
+        {
+            [FieldOffset(0)]
+            public int Int;
+
+            [FieldOffset(0)]
+            public byte byte1;
+            [FieldOffset(1)]
+            public byte byte2;
+            [FieldOffset(2)]
+            public byte byte3;
+            [FieldOffset(3)]
+            public byte byte4;
         }
     }
 }
