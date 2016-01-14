a8021ed70ef6acd0f634eca09f1e5db242b4147b
VladislavZhukovsky <jaymzmanunited@gmail.com>
2016-01-15 02:10:10 +0300

    06 - services

---
+++ 06 - Services/MyService/MyService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyService
+{
+    public class MyService: ServiceBase
+    {
+        internal MyService()
+        {
+            CanStop = true;
+            ServiceName = "MyService";
+            AutoLog = false;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            EventLog.WriteEntry("MyService started");
+        }
+
+        protected override void OnStop()
+        {
+            EventLog.WriteEntry("MyService stopped");
+        }
+    }
+}
