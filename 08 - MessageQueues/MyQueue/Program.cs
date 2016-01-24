db9429bb956e2567511e7864b0a0c5a3aae869f7
VladislavZhukovsky <jaymzmanunited@gmail.com>
2016-01-25 02:17:01 +0300

    08 - Queues project

---
+++ 08 - MessageQueues/MyQueue/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Messaging;
+
+namespace MyQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var queue = new MessageQueue(@"./private$/myqueue"))
+            {
+            }
+        }
+    }
+}
