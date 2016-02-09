ef74b732c32f9a32dc61ea69058e686d23fcc1e0
VladislavZhukovsky <jaymzmanunited@gmail.com>
2016-02-10 01:00:38 +0300

    updates

---
+++ 09 - CustomSerialization/CustomSerizlization/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace CustomSerizlization
+{
+    class Program
+    {
+        static void Main()
+        {
+            var c = new C() { A = "a", B = "b" };
+            var ser = new DataContractSerializer(typeof(C));
+            var fs = new FileStream("file.dat", FileMode.OpenOrCreate);
+            ser.WriteObject(fs, c);
+            fs.Close();
+            fs = new FileStream("file.dat", FileMode.OpenOrCreate);
+            var cc = (C)ser.ReadObject(fs);
+            fs.Close();
+        }
+
+    }
+    [Serializable]
+    public class C
+    {
+        public string A { get; set; }
+        public string B { get; set; }
+        
+        [OnSerializing]
+        public void OnSerializing(StreamingContext context)
+        {
+            A = "on serializing";
+        }
+
+        [OnSerialized]
+        public void OnSerialized(StreamingContext context)
+        {
+            A = "on serialized";
+        }
+    }
+}
